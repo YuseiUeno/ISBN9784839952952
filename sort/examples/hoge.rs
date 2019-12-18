f52114673b5eac90f49877773dfd01994efa3d21
Yusei Ueno <y.say1125@gmail.com>
2019-12-18 11:05:02 +0900

    [#] insertion sort 追加

---
+++ sort/examples/hoge.rs
@@ -0,0 +1,19 @@
+extern crate sort;
+
+use std::io;
+
+fn main() {
+    let mut inputs: Vec<i32> = Vec::new();
+    println!("Please input numbers");
+    for _i in 0..5 {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Filed to read line");
+        let input: i32 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        inputs.push(input);
+    }
+    let sorted = sort::insertion_sort::main(&inputs);
+    println!("{:?}", sorted);
+}
